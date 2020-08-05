129527a9bcfd7c4a817ee134e57a9e67cee3e3ad
Robert Driskell <driskell.ba@gmail.com>
2020-08-04 21:55:09 -0500

    learning about strings, already know most of this stuff

---
+++ ProgrammingFundamentals/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Printf("Programming Fundamentals\n------------------------\n")
+	fmt.Printf("Types\n-----\n")
+	types()
+}
+
+// testing types
+
+var b bool
+
+func types() {
+	fmt.Printf("b is type %T, its value is %v\n", b, b)
+	x := 7
+	z := 8
+	fmt.Println("x == z:", x == z)
+
+	i := 42
+	f := 42.45234
+	fmt.Printf("i: %T\nf: %T\n", i, f)
+
+	var testInt int8
+	// testInt = -129 -- would cause an error, overflows 8-bits signed
+	// testInt = 128 -- Same error
+	testInt = 127
+	fmt.Println(testInt)
+	fmt.Println(runtime.GOARCH)
+
+	s := "Heyo, boyo"
+	bs := []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+}
